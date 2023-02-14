c815ec070eb1ed6c0d5bc8c762386aa61f1cd658
Andrew McOlash <amcolash@gmail.com>
2023-02-14 03:23:43 -0800

    start adding dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import * as React from "react";
+import { Button } from '@mui/material';
+import * as React from 'react';
 
 export default () => (
   <>
     <h1>Welcome to React Parcel Micro App!</h1>
     <p>Hard to get more minimal than this React app.</p>
+    <Button variant="text">Text</Button>
   </>
 );
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBL5N6M93KLcLUjWGacFYaX-Gq5ERcsBWw',
+  authDomain: 'watchlister-a5c59.firebaseapp.com',
+  projectId: 'watchlister-a5c59',
+  storageBucket: 'watchlister-a5c59.appspot.com',
+  messagingSenderId: '1058539063277',
+  appId: '1:1058539063277:web:0bc2f317b4b2d4fe0a89e4',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

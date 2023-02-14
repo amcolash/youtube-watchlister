bd03fe5de3d6df9373d058bcafd7b4cf09ecfae0
Andrew McOlash <amcolash@gmail.com>
2023-02-14 10:52:44 -0800

    simple google signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { Button } from '@mui/material';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import * as React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { app } from './firebase';
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-    <Button variant="text">Text</Button>
-  </>
-);
+export default () => {
+  const auth = getAuth(app);
+  const [user, loading, error] = useAuthState(auth);
+
+  const provider = new GoogleAuthProvider();
+
+  return (
+    <>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+
+      {loading ? (
+        'Loading'
+      ) : user ? (
+        <Button onClick={() => signOut(auth)}>Logout</Button>
+      ) : (
+        <Button onClick={() => signInWithPopup(auth, provider)}>Login</Button>
+      )}
+    </>
+  );
+};
